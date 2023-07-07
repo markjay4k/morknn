e5b15c39a6d5ba9c61d66a50b369e3992be56d09
mark <markjmitry@gmail.com>
2023-07-07 08:02:05 +0000

    training not working yet

--- morknn/net.py
+++ morknn/net.py
@@ -3,21 +3,22 @@ from morknn.layers import Layer
 
 
 class Net:
-    def __init__(self, input_dims, output_dims, activations, dtypes=None, names=None):
-        self.input_dims = input_dims
-        self.output_dims = output_dims
-        self.activations = activations
-        self.n_layers = len(input_dims)
-        self.names = names if names else np.arange(self.n_layers)
-        self.dtypes = dtypes if dtypes else [np.float32] * self.n_layers
-        zipper = zip(
-            self.input_dims, self.output_dims, self.activations, self.dtypes, self.names
-        )
-        self.layers = (
-            Layer(
-                input_dim=i_d, output_dim=o_d, activation=act, dtype=dtype, name=name
-            ) for i_d, o_d, act, dtype, name in zipper
-        )
+    def __init__(self, input_dims=None, output_dims=None, activations=None, dtypes=None, names=None):
+        if input_dims and output_dims and activations:
+            n_layers = len(input_dims)
+            names = names if names else np.arange(n_layers)
+            dtypes = dtypes if dtypes else [np.float32] * n_layers
+            zipper = zip(input_dims, output_dims, activations, dtypes, names)
+            self.layers = (
+                Layer(
+                    input_dim=i_d, output_dim=o_d, activation=act, dtype=dtype, name=name
+                ) for i_d, o_d, act, dtype, name in zipper
+            )
+        else:
+            self.layers = None
+        
+    def define(self, layers):
+        self.layers = layers        
         
     def describe(self):
         "same as print"
@@ -36,15 +37,35 @@ class Net:
         pass
 
     def __repr__(self):
-        name = [str(next(self.layers)) for _ in range(self.n_layers)]
-        return str('\n'.join(name))
+        try:
+            name = [str(layer) for layer in self.layers]
+            return '\n'.join(name)
+        except TypeError as e:
+            if self.layers:
+                name = [str(layer) for layer in self.layers]
+                return str('\n'.join(name))
+            else:
+                return f'<Net: 0 layers>'
+            
 
-    
 if __name__ == '__main__':
+    print(Net())
+    
+
     output_dims = [10, 10, 20, 10]
     input_dims = [train_x.shape[0], *output_dims[1:]]
     activations = ['relu', 'relu', 'relu', 'softmax']
     names = ['input', 'hidden_1', 'hidden_2', 'output']
+    net = Net(output_dims, input_dims, activations)
+    print(net)
+    
 
-    net = Net(input_dims, output_dims, activations, names=names)
-    net.describe()
+    net = Net()
+    layers = [
+        Layer(784, 10, 'relu', dtype=np.float32, name='input'),
+        Layer(10, 10, 'relu', dtype=np.float32, name='hidden_1'),
+        Layer(20, 20, 'relu', dtype=np.float32, name='hidden_2'),
+        Layer(10, 10, 'softmax', dtype=np.float32, name='output'),
+    ]
+    net.define(layers=layers)
+    print(net)
